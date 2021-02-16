dbcf9208a7a7d1871c3a257fd1baba16a30bd7d1
denis <ershgun@mail.ru>
2021-02-16 15:57:22 +0300

    lesson_01.1

--- lesson_01/5.py
+++ lesson_01/5.py
@@ -5,11 +5,10 @@
 
 import subprocess
 
-ping_resurs = [['ping', 'yandex.ru'], ['ping', 'youtube.com']]
-
-for ping_now in ping_resurs:
-
-    ping_process = subprocess.Popen(ping_now, stdout=subprocess.PIPE)
+ping_resource = ['yandex.ru', 'youtube.com']
+for ping_now in ping_resource:
+    args = ['ping', ping_now]
+    ping_process = subprocess.Popen(args, stdout=subprocess.PIPE)
 
     i = 0
 
@@ -23,3 +22,25 @@ for ping_now in ping_resurs:
         else:
             print('= ' * 25)
             break
+
+# Вариант 2
+
+
+# ping_resurs = [['ping', 'yandex.ru'], ['ping', 'youtube.com']]
+#
+# for ping_now in ping_resurs:
+#
+#     ping_process = subprocess.Popen(ping_now, stdout=subprocess.PIPE)
+#
+#     i = 0
+#
+#     for line in ping_process.stdout:
+#
+#         if i < 3:
+#             # print(line)
+#             line = line.decode('cp866').encode('utf-8')
+#             print(line.decode('utf-8'))
+#             i += 1
+#         else:
+#             print('= ' * 25)
+#             break
